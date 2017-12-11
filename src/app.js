1c946cf6d5a3993873b817767440cbdfd8dbf947
Alex Miranda <alexxmirandaa@gmail.com>
2017-12-11 00:07:49 -0200

    Iniciando a estrutura. Trata-se de alguns comandos com npm scripts para buildar e rodar a aplicação.

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import Index from './controllers/index';
+
+let teste = new Index("alex");
+
+teste.init();
---
+++ src/controllers/index.js
@@ -0,0 +1,10 @@
+export default class Index {
+  constructor(name) {
+    this.name = name    
+  };
+
+  init() {
+    console.log(this.name);
+  }
+}
+
